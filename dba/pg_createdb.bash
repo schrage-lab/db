1cc3306aed97cdb0a4f020ddb739ee43d1b1ba0f
Aaron Ward <award7@wisc.edu>
2022-04-01 10:37:55 -0500

    wrapper scripts for createdb

--- dba/pg_createdb.bash
+++ dba/pg_createdb.bash
@@ -1,84 +1,58 @@
 #!/bin/bash
-# create database wrapper
+
+# assumes init_new_db.sql is in same directory
+# run on database 'postgres'
+# ddl = data definition language
+# 		i.e. role that can create and use objects
+# dml = data modeling language 
+# 		i.e. role that can only perform CRUD operations on objects 
+# CRUD = Create Replace Update Delete
 
 function usage(){
-    echo "$(basename $0) -u [USER] -d [DATABASE]"
     echo \
-    "
-    Options:
-        -d, --database      database name
-        -u, --user          user name
-    "
-    }
+    "$(basename $0) USERNAME DATABASE [OPTIONS]
     
+    Required:
+        USERNAME    Username for postgres database that has CREATEDB permissions.
+        DATABASE    Name of new database to create.
+    
+    Options:
+        -h, --help  Show this help dialogue and exit.
+    "
+}
+
 function argparse(){
-    while [ $# -gt 0 ]; do
+    if [ "$#" -lt 2 ]; then
+        usage
+        exit -1
+    fi
+        
+    while [ "$#" -gt 0 ]; do
         case "$1" in
-            -d|--database)
-                DATABASE="$2"
-                shift
-                shift
-                ;;
-            -u|--user)
-                USER="$2"
-                shift
-                shift
-                ;;
             -h|--help)
                 usage
                 exit 0
                 ;;
             *)
-                echo "Invalid argument: $1"
-                usage
-                exit -1
-                ;;
+                shift
         esac
     done
-    
-    [ -z "$USER" ] && echo "No user name provided" && usage && exit -1
-    [ -z "$DATABASE" ] && echo "No database name provided" && usage && exit -1
     }
 
-function createDbSql(){
-    # because this statement cannot exist within a sql function
-    DATABASE="$1"
+function generateSql(){
+    timestamp="$(date +%Y%m%d_%H%M%S)"
+    fname="new_db_tmp_${timestamp}.sql"
+    sql_file="init_new_db.sql"
     
-    echo \
-    "CREATE DATABASE \"$DATABASE\";"
-    }
-
-function revokePublicSql(){
-    DATABASE="$1"
-    
-    echo \
-    "DO
-    \$fn$
-    BEGIN
-        REVOKE ALL ON DATABASE \"$DATABASE\"
-          FROM PUBLIC;
-        REVOKE CREATE
-          ON SCHEMA public
-          FROM PUBLIC;
-    END;
-    \$fn$"
+    sed -e "s/\${VAR}/${1}/" "$sql_file" > "$fname"
+    echo "$fname"
     }
 
 function main(){
-    # define globals/constants
-    USER=""
-    DATABASE=""
-    
-    # todo: this is in a subshell so any exit is not picked up here in the main shell
     argparse "$@"
     
-    # create db
-    sql="$(createDbSql $DATABASE)"
-#    ./psql_executor.bash "$USER" "$sql"
-    
-    # revoke public access
-    sql="$(revokePublicSql $DATABASE)"
-#   ./psql_executor.bash "$USER" "$sql"
+    fname="$(generateSql $2)"
+    psql -U "$1" -d postgres -b -f "$fname" && rm "$fname"
     }
     
 main "$@"
