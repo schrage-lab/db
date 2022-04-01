8e43dffa96360fbf5552fd8b0840afb584fdeaa3
Aaron Ward <award7@wisc.edu>
2022-04-01 10:43:50 -0500

    wrapper scripts for createdb

---
+++ dba/init_new_db.sql
@@ -0,0 +1,45 @@
+-- run as user 'postgres' on database 'postgres'
+-- ddl = data definition language
+-- 		i.e. role that can create and use objects
+-- dml = data modeling language 
+-- 		i.e. role that can only perform CRUD operations on objects 
+-- CRUD = Create Replace Update Delete
+
+CREATE DATABASE ${VAR};
+
+-- revoke 'public' access
+REVOKE ALL 
+	ON DATABASE ${VAR}
+	FROM PUBLIC;
+
+REVOKE CREATE 
+	ON SCHEMA public 
+	FROM PUBLIC;
+
+-- create ddl role
+-- i.e. role that can create objects
+CREATE ROLE db_${VAR}_ddl;
+
+GRANT
+    CONNECT
+    ON DATABASE ${VAR}
+   	TO db_${VAR}_ddl;
+
+GRANT
+    TEMPORARY
+    ON DATABASE ${VAR} 
+   	TO db_${VAR}_ddl;
+
+-- create dml role
+-- i.e. one that only has CRUD options on objects
+CREATE ROLE db_${VAR}_dml;
+
+GRANT
+    CONNECT
+    ON DATABASE ${VAR} 
+   	TO db_${VAR}_dml;
+
+GRANT
+    TEMPORARY
+    ON DATABASE ${VAR} 
+	TO db_${VAR}_dml;
---
+++ dba/pg_createdb.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# assumes init_new_db.sql is in same directory
+# run on database 'postgres'
+# ddl = data definition language
+# 		i.e. role that can create and use objects
+# dml = data modeling language 
+# 		i.e. role that can only perform CRUD operations on objects 
+# CRUD = Create Replace Update Delete
+
+function usage(){
+    echo \
+    "$(basename $0) USERNAME DATABASE [OPTIONS]
+    
+    Required:
+        USERNAME    Username for postgres database that has CREATEDB permissions.
+        DATABASE    Name of new database to create.
+    
+    Options:
+        -h, --help  Show this help dialogue and exit.
+    "
+}
+
+function argparse(){
+    if [ "$#" -lt 2 ]; then
+        usage
+        exit -1
+    fi
+        
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            *)
+                shift
+        esac
+    done
+    }
+
+function generateSql(){
+    timestamp="$(date +%Y%m%d_%H%M%S)"
+    fname="new_db_tmp_${timestamp}.sql"
+    sql_file="init_new_db.sql"
+    
+    sed -e "s/\${VAR}/${1}/" "$sql_file" > "$fname"
+    echo "$fname"
+    }
+
+function main(){
+    argparse "$@"
+    
+    fname="$(generateSql $2)"
+    psql -U "$1" -d postgres -b -f "$fname" && rm "$fname"
+    }
+    
+main "$@"
