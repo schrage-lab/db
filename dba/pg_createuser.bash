e1ffbccf2395641c0ab2d8fc466667b78a5a2fd6
Aaron Ward <award7@wisc.edu>
2022-04-06 13:12:44 -0500

    Create pg_createuser.bash

---
+++ dba/pg_createuser.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function usage(){
+    echo \
+    "$(basename $0) USERNAME [OPTIONS]
+    
+    Required:
+        USERNAME    Username of new user.
+    
+    Options:
+        -h, --help  Show this help dialogue and exit.
+    "
+}
+
+function argparse(){
+    if [ "$#" -lt 1 ]; then
+        usage
+        exit -1
+    fi
+        
+    while [ "$#" -gt 0 ]; do
+        case "$1" in
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            *)
+                shift
+        esac
+    done
+}
+    
+function generateSql(){
+    # $1 = user
+    
+    timestamp="$(date +%Y%m%d_%H%M%S)"
+    sql_file="create_user.sql"
+    tmp_file="/tmp/${sql_file}_tmp_${timestamp}.sql"
+    
+    sed -e "s/\${USER}/${1}/" \
+        "$sql_file" > "$tmp_file"
+    echo "$tmp_file"
+}
+
+function main(){
+    argparse "$@"
+    fname="$(generateSql ${1})"
+    psql -U "$1" -d postgres -b -f "$fname" && rm "$fname"
+}
+
+main "$@"
