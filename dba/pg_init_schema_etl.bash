e7533bec34027e8f48db798412d8a26845659e1c
Aaron Ward <award7@wisc.edu>
2022-04-08 11:52:34 -0500

    added main ETL role creation and granting

--- dba/pg_init_schema_etl.bash
+++ dba/pg_init_schema_etl.bash
@@ -7,12 +7,13 @@ function usage(){
     Wrapper to quickly create new schemas for ETL.
     
     Required:
-        Schema    Name of new schema to create. Five schemas will be created:
-                    SCHEMA_raw
-                    SCHEMA_stage
-                    SCHEMA_prod
-                    SCHEMA_meta
-                    SCHEMA_lookup
+        DATABASE    Name of databse to create schema.
+        SCHEMA      Name of new schema to create. Five schemas will be created:
+                        SCHEMA_raw
+                        SCHEMA_stage
+                        SCHEMA_prod
+                        SCHEMA_meta
+                        SCHEMA_lookup
     
     Options:
         -h, --help  Show this help dialogue and exit.
@@ -37,16 +38,65 @@ function argparse(){
     done
     }
 
-function main(){
-    argparse "$@"
+function createMainRole(){
+    # aggregate rw or ro roles for each schema belonging to an ETL process (e.g. raw, stage, prod...) 
+    # for easier and more robust role granting
     
+    # $1 = role
+    
+    echo \
+    """
+    CREATE
+        ROLE ${1};
+    """
+}
+
+function grantSubRoleToMain(){
+    # $1 = sub-role
+    # $2 = main role
+    
+    echo \
+    """
+    GRANT
+        ${1}
+        TO ${2};
+    """
+}
+
+function main(){
+    # define globals
     DATABASE="$1"
     SCHEMA="$2"
+
+    # parse args
+    argparse "$@"
     
+    # create main rw role for the ETL process
+    MAIN_RW_ROLE="${SCHEMA}_main_rw"
+    sql="$(createMainRole ${ROLE})"
+    psql -d postgres -c "$sql"
+    
+    # create main ro role for the ETL process
+    MAIN_RO_ROLE="${SCHEMA}_main_ro"
+    sql="$(createMainRole ${ROLE})"
+    psql -d postgres -c "$sql"
+    
+    # create schemas and associated rw and ro roles for each
+    # and grant those sub-roles to the main role
     suffixes=("raw" "stage" "prod" "meta" "lookup")
-    for suffix in "${suffixes[@]}"; do
-        ./pg_create_schema.bash "$DATABASE" "${SCHEMA}_${suffix}"
-    done
+    for _suffix in "${suffixes[@]}"; do
+        ./pg_create_schema.bash "$DATABASE" "${SCHEMA}_${_suffix}"
+        
+        # grant SCHEMA_rw to *_main_rw
+        _subrole="${SCHEMA}_${suffix}_rw"
+        sql="$(grantSubRoleToMain ${_subrole} ${MAIN_RW_ROLE})"
+        psql -d postgres -c "$sql"
+        
+        # grant SCHEMA_ro to *_main_ro
+        _subrole="${SCHEMA}_${suffix}_ro"
+        sql="$(grantSubRoleToMain ${_subrole} ${MAIN_RO_ROLE})"
+        psql -d postgres -c "$sql"
+    done    
 }
 
 main "$@"
